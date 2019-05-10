98f79c361ae0be8e8fdae7ee11729e641026de4e
JD <juliannade@gmail.com>
2019-05-10 12:34:40 -0700

    Added cookies and counter

--- buttons.js
+++ buttons.js
@@ -5,7 +5,7 @@ function startAll() {
 	document.getElementById("Everything").onclick = jd_saySomething;
 	document.getElementById("Cinnamon").onclick = jd_saySomething;
 }
-// The main characteristics/ingredients of the bagels 
+// The main characteristics/ingredients of the bagels
 function jd_saySomething() {
 	switch(this.id) {
 		case "Sesame":
@@ -19,4 +19,40 @@ function jd_saySomething() {
 			break;
 		default:
 	}
+}
+
+window.addEventListener("load",initPage,false);
+
+function initPage() {
+	var now = new Date();
+	var expireDate = new Date();
+	expireDate.setMonth(expireDate.getMonth()+6);
+
+	var hitCt = parseInt(cookieVal("pageHit"));
+	hitCt++;
+
+	var lastVisit = cookieVal("pageVisit");
+	if (lastVisit == 0) {
+		lastVisit = "";
+	}
+
+	document.cookie = "pageHit=" + hitCt + ";expires=" + expireDate.toGMTString();
+	document.cookie = "pageVisit=" + now + ";expires=" + expireDate.toGMTString();
+
+	var outMsg = "You have visited this page " + hitCt + " times.";
+	if (lastVisit != "") {
+		outMsg += "<br>Your last visit was " + lastVisit;
+	}
+	document.getElementById("cookieData").innerHTML = outMsg;
+}
+
+function cookieVal(cookieName) {
+	var thisCookie = document.cookie.split("; ");
+
+	for (var i=0; i<thisCookie.length; i++) {
+		if (cookieName == thisCookie[i].split("=")[0]) {
+			return thisCookie[i].split("=")[1];
+		}
+	}
+	return 0;
 }
